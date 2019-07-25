e59cdeb8055c34ef587c7f56b9818c6baea4cd06
Gabriel <gabrielfurlan05@gmail.com>
2019-07-25 18:42:16 -0300

    adding env

--- src/db/index.js
+++ src/db/index.js
@@ -2,11 +2,12 @@ import mongoose from 'mongoose';
 import usersModel from './models/users.model';
 import servicesModel from './models/services.model';
 
+const uri = process.env.MONGO_URL || 'mongodb://localhost/auth-ida';
 const mongodb = async () => {
     mongoose.model('services', servicesModel);
     mongoose.model('users', usersModel);
     mongoose.set('useFindAndModify', false);
-    await mongoose.connect('mongodb://localhost/auth-ida', { useNewUrlParser: true });
+    await mongoose.connect(uri, { useNewUrlParser: true });
 };
 
 export default mongodb;
